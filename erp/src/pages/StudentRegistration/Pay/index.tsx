537a70b265d1fa8658041e67d2c2309405916e88
Your Name <you@example.com>
2022-06-17 11:58:25 +0530

    pay

--- erp/src/pages/StudentRegistration/Pay/index.tsx
+++ erp/src/pages/StudentRegistration/Pay/index.tsx
@@ -32,7 +32,7 @@ const Pay = ({ values, setValues }: PayProps): ReactElement => {
   const [showOtherAmount, setShowOtherAmount] = useState(false)
 
   const filteredDescriptions = values.feeType
-    ? feeDescriptionList.filter(
+    ? feeMasterList.filter(
       (description) => description.title === values.feeType
     )
     : []
